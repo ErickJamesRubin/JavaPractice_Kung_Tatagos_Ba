a2b48e068461c08121103fa6621cb4c136a2b606
ErickJamesRubin <rubin.erick123>
2026-02-07 14:44:38 +0800

    medyo okay sad

--- scanner.java
+++ scanner.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
-public class scanner {
-  public static void main(String [] args){
+class Main {
+  public static void main (String [] args) {
+    Scanner method = new Scanner(System.in);
+    System.out.println("PLease input a destined number of your choice: ");
 
-
+    String username = method.nextLine();
+    System.out.println("Here is your number: " + username);
 
   }
 }
