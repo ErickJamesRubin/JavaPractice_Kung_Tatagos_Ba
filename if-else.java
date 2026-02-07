fc29606535a339e1a0873af1421bd6951aa64408
ErickJamesRubin <rubin.erick123>
2026-02-07 15:05:07 +0800

    Kana jud oh legit

---
+++ if-else.java
@@ -0,0 +1,11 @@
+class IfElse {
+  public static void main(String [] args) {
+    int num = 100;
+
+    if(num > 10){
+      System.out.print("Pangit kay ko og way pulos na pagkatao");
+    } else {
+      System.out.print("Gwapo kay si Royskie");
+    }
+  }
+}
