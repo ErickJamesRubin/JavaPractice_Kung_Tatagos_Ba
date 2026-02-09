098758cb8b4f22e9efe3c83ea4df57e7a6f37823
ErickJamesRubin <rubin.erick123@gmail.com>
2026-02-09 20:07:42 +0800

    added contact to my lesson

---
+++ concat.java
@@ -0,0 +1,8 @@
+public class concat {
+  public static void main (String [] args) {
+    String firstName = "Erick James";
+    String lastName = "Rubin";
+
+    System.out.print(firstName.concat (lastName));
+  }
+}
