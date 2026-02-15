18d24412d1e55f5636f97393eeb09d86685edab9
ErickJamesRubin <rubin.erick123@gmail.com>
2026-02-15 10:15:19 +0800

    Add code

--- Valentines.java
+++ Valentines.java
@@ -1,3 +1,12 @@
 public class Valentines {
-  
+  public static void main (String [] args) {
+  int num;
+  String ily;
+  ily = "I Love You";
+  num = 0;
+    while(num <= 999999) {
+      System.out.println(num + ily);
+      num++;
+    }
+    }
 }
